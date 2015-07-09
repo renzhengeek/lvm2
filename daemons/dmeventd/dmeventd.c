534ad021e9e449d85ca20fbc58d4397993c76434
Liuhua Wang <lwang@suse.com>
2015-07-09 15:44:20 +0800

    dmeventd: fix dso name wrong compare
    dso_name is thought as not empty and doesn't create monitor thread,
    because lvm sets empty dso_name with "-".
    
    Signed-off-by: Liuhua Wang <lwang@suse.com>
    References: bnc#935623

--- daemons/dmeventd/dmeventd.c
+++ daemons/dmeventd/dmeventd.c
@@ -1218,12 +1218,12 @@ static int _want_registered_device(char *dso_name, char *device_uuid,
 				   struct thread_status *thread)
 {
 	/* If DSO names and device paths are equal. */
-	if (dso_name && device_uuid)
+	if (dso_name && strcmp(dso_name, "-") && device_uuid)
 		return !strcmp(dso_name, thread->dso_data->dso_name) &&
 		    !strcmp(device_uuid, thread->device.uuid);
 
 	/* If DSO names are equal. */
-	if (dso_name)
+	if (dso_name && strcmp(dso_name, "-"))
 		return !strcmp(dso_name, thread->dso_data->dso_name);
 
 	/* If device paths are equal. */
