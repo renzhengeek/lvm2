878e57ffa289673985066e5931902b389da70889
Eric Ren <zren@suse.com>
2017-02-07 14:48:27 +0800

    add libdm-iface-not-output-error-message-inside-retry-loop.patch

--- libdm/ioctl/libdm-iface.c
+++ libdm/ioctl/libdm-iface.c
@@ -52,6 +52,7 @@
 #define MISC_NAME "misc"
 
 #define NUMBER_OF_MAJORS 4096
+#define DM_IOCTL_RETRIES 25
 
 /*
  * Static minor number assigned since kernel version 2.6.36.
@@ -1735,7 +1736,7 @@ static struct dm_ioctl *_do_dm_ioctl(struct dm_task *dmt, unsigned command,
 					    "failed: %s",
 				    	    _cmd_data_v4[dmt->type].name,
 					    strerror(errno));
-			else
+			else if(errno != EBUSY || retry_repeat_count == DM_IOCTL_RETRIES)
 				log_error("device-mapper: %s ioctl on %s "
 					  "failed: %s",
 					  _cmd_data_v4[dmt->type].name,
@@ -1780,7 +1781,6 @@ void dm_task_update_nodes(void)
 	update_devs();
 }
 
-#define DM_IOCTL_RETRIES 25
 #define DM_RETRY_USLEEP_DELAY 200000
 
 int dm_task_run(struct dm_task *dmt)
